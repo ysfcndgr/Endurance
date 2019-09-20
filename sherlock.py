ec1cb5b20868e93972ee71e2e2f63a9fea02c038
ysfcndgr <32979760+ysfcndgr@users.noreply.github.com>
2019-09-20 19:31:51 +0300

    Update sherlock.py

--- sherlock.py
+++ sherlock.py
@@ -8,22 +8,23 @@ from headless import headlessopen
 
 class sherlockClass():
     def sherlock(self,aranacakisi):
-        print("[*] 23 platformdan aynı anda veri çekiliyor bu yüzden sherlock yavaş çalışır...")
-        print("[*] Tahmini tamamlanma süresi : 2 dakika olarak belirlendi...")
+        print("[*] Sherlock retrieving data from 23 different platform that's why it may work slowly...")
+        print("[*] Estimated completion time: Set to 2 minutes...")
         self.browser_yakala=headlessopen().headlessopenwith()
         #time.sleep(10)
         """
-        selenium eski sürümlerinde kod satırı tamamlanmasa bile diğer işlemlere geçiyordu
-        bu yüzden kod hata veriyordu ancak yeni sürümünlerinde: Benim kullandığım (3.141.0) böyle bir 
-        davranışta bulunmuyor eğer program işlerken hata alıyorsanız time.sleep(10) fonksiyonunu ve
-        aşağıdaki time.sleep(5) fonksiyonunu yorum satırından çıkarın.
+        In the old version of Selenium even if line of code does not complete, it pass the next process.
+        Therefore the code was giving an error but in the new version:
+        The version that i am using (3.141.0) you will not face with those kind of errors.
+        If you receive any error while program is working;
+        You need to remove time.sleep(10) and time.sleep(5) function from comment line.
         """
         self.browser_yakala.get("http://www.usersherlock.com/")
        
         self.veriyial = self.browser_yakala.find_element_by_css_selector('#content > div.search > form > input[type=text]:nth-child(1)')
         self.veriyial.click()
         self.veriyial.send_keys(aranacakisi)
-        print("[*] Sabret az kaldı...")
+        print("[*] Almost done...")
         self.tikla = self.browser_yakala.find_element_by_xpath('//*[@id="content"]/div[2]/form/input[2]')
         self.tikla.click()
         #time.sleep(5)
@@ -33,22 +34,22 @@ class sherlockClass():
         self.ciktilariyazdir()
         self.browser_yakala.quit()    
     def ciktilariyazdir(self):  
-        self.yazdirmakistiyormusun = input("Verileri dosyaya yazdırmak istiyor musunuz? E/H ")
-        if self.yazdirmakistiyormusun.upper() == "E":
-            self.dosya_ismi = input("Dosya ismini girmelisin: ")
-            with open("veriler/"+self.dosya_ismi+".txt","w",encoding="UTF-8") as file:
+        self.yazdirmakistiyormusun = input("Do you want to print the data to the file Y/N ? ")
+        if self.yazdirmakistiyormusun.upper() == "Y":
+            self.dosya_ismi = input("You have to enter the file name: ")
+            with open("data/"+self.dosya_ismi+".txt","w",encoding="UTF-8") as file:
                 for bilgi in self.liste:
                     file.write(bilgi)
                     file.write("**************************")
-            print("[*] Bilgiler başarıyla yazıldı...")
-        elif self.yazdirmakistiyormusun.upper() == "H":
-            print("[*] Dosyaya veri yazılmadı programdan çıkılıyor...")
+            print("[*] Information successfully written...")
+        elif self.yazdirmakistiyormusun.upper() == "N":
+            print("[*] Data not written to the file...Exiting the program...")
         else:
-            print("Yanlış giriş")
+            print("incorrect entry")
     
     def ekranayazdir(self):
         for element in self.elements:
-            print("---------------Sonuçlar Aşağıda Listelenecek---------------")
+            print("---------------The results will be listed below---------------")
             self.liste.append(element.text)
             print(element.text)
     def __str__(self):
